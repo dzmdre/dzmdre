a2228f7f19bb570021df05c3a4736856fbce6cad
dmitry <lixefer@gmail.com>
2025-11-19 22:25:34 +0300

    updates quotes

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ async function main() {
         await fs.readFile(path.join(process.cwd(), "./README.template.md"))
     ).toString("utf-8");
 
-    // const quote = await (
-    //     await fetch("https://quoteslate.vercel.app/api/quotes/random?tags=knowledge")
-    // ).json();
+    const quote = await (
+        await fetch("https://dummyjson.com/quotes/1")
+    ).json();
 
-    // console.log(quote);
+    console.log(quote);
 
-    // const readme = readmeTemplate
-    //     .replace("{quote}", quote.quote)
-    //     .replace("{quote_author}", `- ${quote.author}`)
+    const readme = readmeTemplate
+        .replace("{quote}", quote.quote)
+        .replace("{author}", `- ${quote.author}`)
 
-    //await fs.writeFile("README.md", readme);
-    await fs.writeFile("README.md", readmeTemplate);
+    await fs.writeFile("README.md", readme);
+    // await fs.writeFile("README.md", readmeTemplate);
 }
 
 main();
